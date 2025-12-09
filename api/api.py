25ec49eeb2eb47b338a2c1902b35203b5df22a39
lyxxng <lisa.shan.young@gmail.com>
2025-12-08 21:34:15 -0800

    Add API endpoint for running SAMFEO

--- api/api.py
+++ api/api.py
@@ -1,9 +1,60 @@
+from flask import Flask, request, jsonify
+import subprocess
 import time
-from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/api/submit', methods=['POST'])
+def samfeo_submission():
+    body = request.json
 
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
+    structure = body.get("structure")
+    temperature = body.get("temperature")
+    queue = body.get("queue")
+    step = body.get("step")
+    obj = body.get("object")
+    init = body.get("init")
+    nosm = body.get("nosm")
+    nomfe = body.get("nomfe")
+
+    # Create list of arguments
+    args = ["--online", "--t", temperature, "--k", queue, "--object", obj, "--init", init, "--step", step]
+
+    if nosm:
+        args.append("--nosm")
+    if nomfe:
+        args.append("--nomfe")
+
+    # Run SAMFEO with timer
+    start_time = time.time()
+
+    result = subprocess.run(
+        ["python3", "../SAMFEO/main.py"] + args,
+        input=structure,
+        text=True,
+        capture_output=True)
+    
+    end_time = time.time()
+    
+    s = result.stdout
+    e = result.stderr
+
+    # If contents in stderr return code 400
+    if e:
+        return jsonify({
+            "error": e
+        }), 400
+
+    # Located the RNA sequence in stdout
+    start_s = s.find("RNA sequence")
+    end_s = s.find("ensemble objective:  ")
+    seq = s[start_s + 15:end_s - 1]
+
+    total_time = end_time - start_time
+
+    # Return structure input, sequence, and time elapsed
+    return jsonify({
+        "structure": structure,
+        "rna": seq,
+        "time": total_time
+    })
