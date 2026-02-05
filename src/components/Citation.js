625f427ce07a95a5fb9a9663e3b0477c4f507d20
lyxxng <lisa.shan.young@gmail.com>
2026-02-04 21:46:35 -0800

    modify citation format

--- src/components/Citation.js
+++ src/components/Citation.js
@@ -8,10 +8,10 @@ export default function Citation() {
             <h4>Please cite the following paper:</h4>
 
             <h6>Tianshou Zhou, Ning Dai, Sizhen Li, Max Ward, David H. Matthews, and Liang Huang (2023).&nbsp;
-            <a href="https://academic.oup.com/bioinformatics/article/39/Supplement_1/i563/7210514">
-            RNA Design via Structure-Aware Multi-Frontier Ensemble Optimization</a>.&nbsp;
-            <i>Bioinformatics</i>, Volume 39, Issue Supplement_1, July 2023, Pages i563-i571.</h6>
-            
+                <a href="https://academic.oup.com/bioinformatics/article/39/Supplement_1/i563/7210514">
+                RNA design via structure-aware multifrontier ensemble optimization</a>.&nbsp;
+                <i>Bioinformatics</i>, 39(Supplement_1), i563–i571.</h6>
+                
             <br></br>
 
             <h6>Code for SAMFEO:&nbsp;
