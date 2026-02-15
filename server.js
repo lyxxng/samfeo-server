ab6621ff86c6007031a3ff447deb9475a2a99bec
lyxxng <lisa.shan.young@gmail.com>
2026-02-14 17:57:39 -0800

    fix environment variables

--- server.js
+++ server.js
@@ -5,12 +5,16 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 const app = express();
 const PORT = 8083;
 
+const API_URL = process.env.REACT_APP_API_URL;
+const BASENAME = process.env.REACT_APP_BASENAME;
+const PROXY_URL = process.env.PROXY_URL;
+
 // Proxy requests to flask backend
-app.use('/samfeo/api', createProxyMiddleware({
-  target: 'http://10.217.112.20:5002',
+app.use(`${API_URL}`, createProxyMiddleware({
+  target: PROXY_URL,
   changeOrigin: true,
   pathRewrite: {
-    '^/samfeo/api': '/api'  // Strip the /samfeo prefix
+    [`^${API_URL}`]: '/api'  // Strip the basename prefix and replace with /api
   },
   proxyTimeout: 900000,
   timeout: 900000,
@@ -19,16 +23,16 @@ app.use('/samfeo/api', createProxyMiddleware({
 // Absolute path to build folder
 const buildPath = path.join(__dirname, "build");
 
-// Serve static assets under /samfeo
-app.use("/samfeo", express.static(buildPath));
+// Serve static assets
+app.use(BASENAME, express.static(buildPath));
 
 // SPA fallback
-app.get("/samfeo/*", (req, res) => {
+app.get(`${BASENAME}/*`, (req, res) => {
   res.sendFile(path.join(buildPath, "index.html"));
 });
 
 const server =  app.listen(PORT, "0.0.0.0", () => {
-  console.log(`App running at http://127.0.0.1:${PORT}/samfeo/`);
+  console.log(`App running at http://127.0.0.1:${PORT}/${BASENAME}/`);
 });
 
 server.timeout = 900000;
